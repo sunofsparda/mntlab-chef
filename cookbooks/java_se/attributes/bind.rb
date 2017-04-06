17084608b1205751bf304c61a8731ee063749e54
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-06 16:27:05 +0300

    init jboss cookbook

---
+++ cookbooks/java_se/attributes/bind.rb
@@ -0,0 +1,11 @@
+default['java_se']['release'] = '8'
+default['java_se']['update'] = '121'
+default['java_se']['build'] = '13'
+default['java_se']['hash'] = 'e9e7ea248e2c4826b92b3f075a80e441'
+
+# https://www.oracle.com/webfolder/s/digest/8u121checksum.html
+default['java_se']['sha256']['tar']['i586'] = 'f7d6cf1468c5e71ff097bec0189caccdd8e709a2a88a2c9849ad6200c0f33d4c'
+default['java_se']['sha256']['tar']['x64'] = '97e30203f1aef324a07c94d9d078f5d19bb6c50e638e4492722debca588210bc'
+default['java_se']['sha256']['dmg']['x64'] = '82ff2493cd4b9ebdaeb9135abaffc9a37b71d341b007a83f73aa6ff3df1b6a3a'
+default['java_se']['sha256']['exe']['i586'] = 'b57abcfbcdd42d15626775fb214811328f614a9a9e623d4904bbfd6bb2aac79f'
+default['java_se']['sha256']['exe']['x64'] = '3e39f87441841a152cd327e95d09755fbe4b443316dac94a0494d68910b0994e'
