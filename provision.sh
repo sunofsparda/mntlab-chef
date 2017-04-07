9eeb366cce47abc9de4b2c87102187d5efd7193c
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-07 09:26:17 +0300

    correct provision

--- provision.sh
+++ provision.sh
@@ -8,7 +8,7 @@ rm /root/chef_cookbooks/* -rf
 
 # echo '=============== INIT ==============='
 # echo 'Preparing environment'
-yum install -y git
+# yum install -y git
 
 echo '============= INIT_CHEF ============='
 echo 'Installing Chef and ChefDK'
@@ -19,7 +19,7 @@ mkdir -p /root/chef_cookbooks
 
 # echo '=========== GET_COOKBOOKS ==========='
 # echo 'Downloading cookbooks'
-git clone https://github.com/agileorbit-cookbooks/java.git /root/chef_cookbooks/java 
+# git clone https://github.com/agileorbit-cookbooks/java.git /root/chef_cookbooks/java 
 # git clone https://github.com/dhoer/chef-java_se.git /root/chef_cookbooks/chef-java_se
 # git clone https://github.com/chef-cookbooks/tar.git /root/chef_cookbooks/tar
 # # git clone https://github.com/chef-cookbooks/chef_nginx.git /root/chef_cookbooks/chef_nginx
@@ -71,9 +71,10 @@ git clone https://github.com/agileorbit-cookbooks/java.git /root/chef_cookbooks/
 # tar -xf cookbooks-*.tar.gz -C /root/
 # rm cookbooks-*tar.gz
 
-# echo '=========== GET_EXISTING_COOKBOOKS ==========='
-# cp -r /vagrant/.chef /root/
-# cp -r /vagrant/cookbooks /root/
+echo '=========== GET_EXISTING_COOKBOOKS ==========='
+cp -r /vagrant/.chef /root/
+cp -r /vagrant/cookbooks /root/
 
+chef-solo -c /root/.chef/solo.rb > /vagrant/output.log
 
 echo 'exit 0'
