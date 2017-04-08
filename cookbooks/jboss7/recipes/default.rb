6c3060b28fd91f2ac6dda0026ddd996ccbdca1a4
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-08 11:56:18 +0300

    test1

--- cookbooks/jboss7/recipes/default.rb
+++ cookbooks/jboss7/recipes/default.rb
@@ -56,30 +56,30 @@ template "#{node['jboss7']['jboss_home']}/bin/standalone.conf" do
   group node['jboss7']['jboss_group']
   mode '0644'
   notifies :restart, 'service[jboss7]', :delayed
-# end
+end
 
 dist_dir, _conf_dir = value_for_platform_family(
   ['debian'] => %w( debian default ),
   ['rhel'] => %w( redhat sysconfig )
 )
 
-# template '/etc/jboss-as.conf' do
-#   source "#{dist_dir}/jboss-as_conf.erb"
-#   mode 0775
-#   owner 'root'
-#   group node['root_group']
-#   only_if { platform_family?('rhel') }
-#   notifies :restart, 'service[jboss7]', :delayed
-# end
+template '/etc/jboss-as.conf' do
+  source "#{dist_dir}/jboss-as_conf.erb"
+  mode 0775
+  owner 'root'
+  group node['root_group']
+  only_if { platform_family?('rhel') }
+  notifies :restart, 'service[jboss7]', :delayed
+end
 
-# template '/etc/init.d/jboss7' do
-#   source "#{dist_dir}/jboss7-init.erb"
-#   mode 0775
-#   owner 'root'
-#   group node['root_group']
-#   notifies :enable, 'service[jboss7]', :delayed
-#   notifies :restart, 'service[jboss7]', :delayed
-# end
+template '/etc/init.d/jboss7' do
+  source "#{dist_dir}/jboss7-init.erb"
+  mode 0775
+  owner 'root'
+  group node['root_group']
+  notifies :enable, 'service[jboss7]', :delayed
+  notifies :restart, 'service[jboss7]', :delayed
+end
 
 # jboss7_user node['jboss7']['admin_user'] do
 #   password node['jboss7']['admin_pass']
@@ -87,7 +87,7 @@ dist_dir, _conf_dir = value_for_platform_family(
 #   notifies :restart, 'service[jboss7]', :delayed
 # end
 
-# service 'jboss7' do
-#   supports restart: true
-#   action :enable
-# end
+service 'jboss7' do
+  supports restart: true
+  action :enable
+end
