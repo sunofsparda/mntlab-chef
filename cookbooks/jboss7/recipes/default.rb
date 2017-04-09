90826618916bbc776b2290dfaf645caa70f41507
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-09 13:51:31 +0300

    removed java

--- cookbooks/jboss7/recipes/default.rb
+++ cookbooks/jboss7/recipes/default.rb
@@ -4,8 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-
-include_recipe 'java'
 include_recipe "jboss7::install"
 include_recipe "jboss7::configure"
 include_recipe "jboss7::deploy"
