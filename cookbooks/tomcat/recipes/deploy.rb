69cfae9f048b22cb334a9444ddf04c2653ca0698
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-11 13:12:29 +0300

    2

--- cookbooks/tomcat/recipes/deploy.rb
+++ cookbooks/tomcat/recipes/deploy.rb
@@ -15,22 +15,18 @@ remote_file node['tomcat']['test_app_archive'] do
   action :create_if_missing
 end
 
-bash 'deploy application' do
-  code <<-EOF
-    unzip #{node['tomcat']['test_app_archive']} -d #{node['tomcat']['temp']}
-    mv #{node['tomcat']['temp']}/#{node['tomcat']['test_app']}/* #{node['tomcat']['tomcat_deploy']}/
-    rm -rf #{node['tomcat']['temp']}/#{node['tomcat']['test_app']}
-  EOF
+execute 'deploy application' do
+  command "unzip #{node['tomcat']['test_app_archive']} -d #{node['tomcat']['temp']}"
+  owner node['tomcat']['tomcat_user']
+  group node['tomcat']['tomcat_group']
+  creates "#{node['tomcat']['temp']}/#{node['tomcat']['test_app']}/#{node['tomcat']['test_app']}.war"
+  action :create_if_missing
+end 
+
+remote_file "#{node['tomcat']['tomcat_deploy']}/#{node['tomcat']['test_app']}.war"
+  source "file://#{node['tomcat']['temp']}/#{node['tomcat']['test_app']}/#{node['tomcat']['test_app']}.war"
   user 'root'
   group node['tomcat']['tomcat_group']
   creates "#{node['tomcat']['test_app_check']}"
   notifies :restart, 'service[tomcat]', :immediately
-end 
-
-# remote_file "#{node['tomcat']['tomcat_deploy']}/#{node['tomcat']['test_app']}.war"
-#   source "#{node['tomcat']['temp']}/#{node['tomcat']['test_app']}.war/"
-#   user 'root'
-#   group node['tomcat']['tomcat_group']
-#   creates "#{node['tomcat']['test_app_check']}"
-#   notifies :restart, 'service[tomcat]', :immediately
-# end
+end
