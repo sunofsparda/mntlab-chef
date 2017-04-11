27efe0dc6b3059c99582171acae177412840bcc7
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-11 16:18:39 +0300

    rewriten knife and bootstrap.sh

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,12 +1,22 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-validation_client_name   'chef-validator'
-validation_key           "#{current_dir}/#{validation_client_name}.pem"
-chef_server_name         'chef-server'
-chef_server_url          "https://#{chef_server_name}"
-node_name                'chef-node1'
-client_key               "#{current_dir}/#{node_name}.pem"
-chef_node_url            "#{chef_server_url}/organizations/#{node_name}"
-cookbook_path            "#{current_dir}/../cookbooks"
-syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
+node_name                "acherlyonok"
+client_key               "#{current_dir}/acherlyonok.pem"
+chef_server_url          "https://chef-server/organizations/acsun"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
+# current_dir = File.dirname(__FILE__)
+# log_level                :info
+# log_location             STDOUT
+# validation_client_name   'chef-validator'
+# validation_key           "#{current_dir}/#{validation_client_name}.pem"
+# chef_server_name         'chef-server'
+# chef_server_url          "https://#{chef_server_name}"
+# node_name                'chef-node1'
+# client_key               "#{current_dir}/#{node_name}.pem"
+# chef_node_url            "#{chef_server_url}/organizations/#{node_name}"
+# cookbook_path            "#{current_dir}/../cookbooks"
+# syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
--- bootstrap_provision.sh
+++ bootstrap_provision.sh
@@ -2,8 +2,10 @@
 
 knife ssl fetch
 knife ssl check
-knife cookbook  upload --all
-# knife environment create TEST_NODE -d "Environment for development cookbooks"
-# knife environment list
 
-knife bootstrap 192.168.100.102 -x root -P 'vagrant' -N TEST_NODE -E chef-node1 -r 'jboss'
+knife cookbook  upload --all
+
+knife bootstrap 192.168.100.102 -N node-chef1 -x root -P 'vagrant'
+
+knife client list
+
