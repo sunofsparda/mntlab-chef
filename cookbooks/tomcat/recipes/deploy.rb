9b6a647fb9100fbeadc93b476ebe68b12513e170
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-11 13:16:44 +0300

    fixed

--- cookbooks/tomcat/recipes/deploy.rb
+++ cookbooks/tomcat/recipes/deploy.rb
@@ -17,13 +17,13 @@ end
 
 execute 'deploy application' do
   command "unzip #{node['tomcat']['test_app_archive']} -d #{node['tomcat']['temp']}"
-  owner node['tomcat']['tomcat_user']
+  user node['tomcat']['tomcat_user']
   group node['tomcat']['tomcat_group']
   creates "#{node['tomcat']['temp']}/#{node['tomcat']['test_app']}/#{node['tomcat']['test_app']}.war"
   action :create_if_missing
 end 
 
-remote_file "#{node['tomcat']['tomcat_deploy']}/#{node['tomcat']['test_app']}.war"
+remote_file "#{node['tomcat']['tomcat_deploy']}/#{node['tomcat']['test_app']}.war" do
   source "file://#{node['tomcat']['temp']}/#{node['tomcat']['test_app']}/#{node['tomcat']['test_app']}.war"
   user 'root'
   group node['tomcat']['tomcat_group']
