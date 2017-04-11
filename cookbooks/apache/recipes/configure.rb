d1d258564e57688dfe83f47d89566e9a7c16f9d2
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-11 15:59:22 +0300

    removed wrong block from apache::configure

--- cookbooks/apache/recipes/configure.rb
+++ cookbooks/apache/recipes/configure.rb
@@ -9,10 +9,3 @@ template '/etc/httpd/conf.d/vhost.conf' do
   mode '0644'
   notifies :restart, 'service[httpd]', :immediately
 end
-
-remote_file node['tomcat']['test_app_archive'] do
-  source node['tomcat']['test_app_url']
-  owner node['tomcat']['tomcat_user']
-  group node['tomcat']['tomcat_group']
-  action :create_if_missing
-end
