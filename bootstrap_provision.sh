d61e6af3c1158813633efa5f1bb3133db6f01ba7
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-11 20:51:22 +0300

    added creating env and bag

--- bootstrap_provision.sh
+++ bootstrap_provision.sh
@@ -9,3 +9,7 @@ knife bootstrap 192.168.100.102 -N node-chef1 -x root -P 'vagrant'
 
 knife client list
 
+#knife environment create Chef-dev -e vim
+knife environment from file chef-dev.json
+
+knife data bag create newbag
---
+++ cookbooks/hudson/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: hudson
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
