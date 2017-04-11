0365f05dbd765f34781a1d3d7a914f3d0d5f5e3f
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-11 22:48:43 +0300

    ready for pull request

--- bootstrap_provision.sh
+++ bootstrap_provision.sh
@@ -16,7 +16,7 @@ knife environment from file chef-dev_env.json
 knife data bag from file newbag hudson_databag.json
 
 # knife role create jboss_role -e vim
-knife role from file jboss_role.json # NOT WORKING
+knife role from file jboss_role.json
 
 # knife role delete $role_name # to delete role
 knife bootstrap 192.168.100.102 -N chef-node1 -x root -P vagrant -r 'role[jboss]' -E "Chef-dev"
